62c70604b9dd5147b53e0d35f5e5991cc4007937
Master Inspire <masterinspire@outlook.com>
2018-04-16 22:35:12 -0800

    to start over

---
+++ build/build.bat
@@ -0,0 +1,5 @@
+@echo off
+
+cd ../src/
+dotnet build --configuration Release
+pause
---
+++ src/TheOne.RabbitMq.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using NUnit.Framework;
+
+namespace TheOne.RabbitMq.Tests {
+
+    [TestFixture]
+    internal sealed class UnitTest1 {
+
+        [Test]
+        public void TestMethod1() { }
+    }
+}
