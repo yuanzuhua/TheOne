1a8940d69e96f8b761bd70b4629c84acf6ae8638
Master Inspire <masterinspire@outlook.com>
2019-02-07 20:24:10 -0800

    redis: change DeactivatedClientsExpiry to 0 seconds

--- src/TheOne.Redis/RedisConfig.cs
+++ src/TheOne.Redis/RedisConfig.cs
@@ -79,10 +79,10 @@ namespace TheOne.Redis {
         public static int? AssumeServerVersion;
 
         /// <summary>
-        ///     How long to hold deactivated clients for before disposing their connection (default 10 seconds)
+        ///     How long to hold deactivated clients for before disposing their connection (default 0 seconds)
         ///     Dispose of deactivated Clients immediately with TimeSpan.Zero
         /// </summary>
-        public static TimeSpan DeactivatedClientsExpiry = TimeSpan.FromSeconds(10);
+        public static TimeSpan DeactivatedClientsExpiry = TimeSpan.Zero;
 
         /// <summary>
         ///     Whether Debug Logging should log detailed Redis operations (default false)
@@ -130,7 +130,7 @@ namespace TheOne.Redis {
             VerifyMasterConnections = true;
             HostLookupTimeoutMs = 200;
             AssumeServerVersion = null;
-            DeactivatedClientsExpiry = TimeSpan.FromSeconds(10);
+            DeactivatedClientsExpiry = TimeSpan.Zero;
             EnableVerboseLogging = false;
             CertificateSelectionCallback = null;
             CertificateValidationCallback = null;
